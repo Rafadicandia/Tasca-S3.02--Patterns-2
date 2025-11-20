d2933ec13324b92125c87268949194cca6413f8e
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-11-20 12:26:52 +0100

    build main for testing

--- Nivell1/Builder/src/main/java/Main.java
+++ Nivell1/Builder/src/main/java/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
+        Builder builder = new Builder();
+
+        MestrePizzer cheff = new MestrePizzer();
+
+        cheff.builderMargarita(builder);
+
+        
+
+
+
+
 
 
 
