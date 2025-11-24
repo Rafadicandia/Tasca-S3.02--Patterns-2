146553a5a2df59aad184adb4f064525d75497889
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-11-24 10:28:56 +0100

    build

--- Nivell2/Observer/src/main/java/StockX.java
+++ Nivell2/Observer/src/main/java/StockX.java
@@ -1,15 +1,25 @@
 public class StockX {
     private int value;
+    private Broker info;
 
-    public StockX(int value) {
+    public StockX(int value, Broker info) {
         this.value = value;
+        this.info = info;
     }
 
     public int getValue() {
         return value;
     }
 
-    public void setValue(int value) {
+    private void setValue(int value) {
         this.value = value;
+    }
+
+    public Broker getInfo() {
+        return info;
+    }
+
+    private void setInfo(Broker info) {
+        this.info = info;
     }
 }
