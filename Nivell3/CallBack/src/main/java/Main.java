13d00f91a272b1ab760dbdf5f8b28ac9f2ced4a1
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-11-24 13:46:29 +0100

    build

--- Nivell3/CallBack/src/main/java/Main.java
+++ Nivell3/CallBack/src/main/java/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
         Store adidas = new Store("Adidas");
         Bizum bizumPayment = new Bizum("+34-634-330-718");
+        CreditCard visa = new CreditCard("456-213-678-0987");
         bizumPayment.executeWith(adidas);
+        visa.executeWith(adidas);
 
 
     }
