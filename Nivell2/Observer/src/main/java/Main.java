896bb5ff60a339e43cabbdb835def911a2b125e0
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-11-24 11:33:26 +0100

    build

--- Nivell2/Observer/src/main/java/Main.java
+++ Nivell2/Observer/src/main/java/Main.java
@@ -1,8 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         Agency phenix = new Agency("Phenix Stocks");
+        Agency stateHouse = new Agency("State House");
+
+
+
         Broker shark = new Broker("up", "down");
         shark.subscribe("up", phenix);
+        shark.subscribe("up", stateHouse);
+        shark.subscribe("down", stateHouse);
         shark.notifyUpdate("up", "Nvidia -> 5% increase");
+        shark.notifyUpdate("down", "Tesla->-8%");
     }
 }
