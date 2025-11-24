f5eca17096c3d746c06119cddc3968a894952946
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-11-24 10:23:02 +0100

    build

--- Nivell2/Observer/src/main/java/Broker.java
+++ Nivell2/Observer/src/main/java/Broker.java
@@ -5,7 +5,22 @@ import java.util.Map;
 public class Broker {
     Map<String, List<EventListener>> subscribers = new HashMap<>();
 
-    
+    public void subscribe(String eventType, EventListener listener) {
+        List<EventListener> users = listeners.get(eventType);
+        users.add(listener);
+    }
+
+    public void unsubscribe(String eventType, EventListener listener) {
+        List<EventListener> users = listeners.get(eventType);
+        users.remove(listener);
+    }
+
+    public void notify(String eventType, File file) {
+        List<EventListener> users = listeners.get(eventType);
+        for (EventListener listener : users) {
+            listener.update(eventType, file);
+        }
+    }
 }
 
 
---
+++ Nivell2/Observer/src/main/java/StockX.java
@@ -0,0 +1,15 @@
+public class StockX {
+    private int value;
+
+    public StockX(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
