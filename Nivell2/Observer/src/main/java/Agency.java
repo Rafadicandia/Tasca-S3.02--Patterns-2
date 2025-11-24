f16792079facc59611fdbdbb182ca3493da4f196
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-11-24 11:16:31 +0100

    build

--- Nivell2/Observer/src/main/java/Agency.java
+++ Nivell2/Observer/src/main/java/Agency.java
@@ -1,7 +1,13 @@
-public class Agency implements EventListener{
+public class Agency implements EventListener {
+    private String name;
+
+    public Agency(String name) {
+        this.name = name;
+    }
 
     @Override
     public void updateStock(String message) {
+        System.out.println(name + " agency just received this stock update: " + message);
 
     }
 }
